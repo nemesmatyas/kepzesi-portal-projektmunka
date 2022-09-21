b2a870b6c00f15fd9b30da3a841a1c9b44570756
nemes <nemesmatyas@gmail.com>
2022-09-21 14:44:54 +0200

    KPI oldal alapok kész

---
+++ src/components/AktualisNegyedev.js
@@ -0,0 +1,37 @@
+const AktualisNegyedev = () => {
+  return (
+    <div>
+      <h2>Aktuális negyedév</h2>
+      <p>Periódus: </p>
+      <p>2022 Q4</p>
+      <p>Alap mozgóbér:</p>
+      <p>150 000 HUF</p>
+      <p>Állapot:</p>
+      <p>Folyamatban</p>
+      <p>Eddigi eredmény:</p>
+      <p>60%</p>
+      <h3>Kitűzött célok:</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Név</th>
+            <th>Súly</th>
+            <th>Leírás</th>
+            <th>Felhasználói megjegyzés</th>
+            <th>Jóváhagyói megjegyzés</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Visszajelzés az ügyféltől</td>
+            <td>0,2</td>
+            <td>Kötelező KPI</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+export default AktualisNegyedev;
