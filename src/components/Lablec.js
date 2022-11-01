5c06491a06e1e68cc9a2db4627c6e18911ef33fd
nemes <nemesmatyas@gmail.com>
2022-11-01 19:58:01 +0100

    Bence autentikációs modulja merge-elve a mainnel

--- src/components/Lablec.js
+++ src/components/Lablec.js
@@ -2,8 +2,8 @@ import classes from "./Lablec.module.css";
 
 const Lablec = () => {
     return <div className={classes.lablec}>
-        <p1>Készítette: Nemes Mátyás, Homlok Bence, Rózsa László</p1>
-        <p2>&copy; 2022, Széchenyi István Egyetem</p2> 
+        <span>Készítette: Nemes Mátyás, Homlok Bence, Rózsa László</span>
+        <span>&copy; 2022, Széchenyi István Egyetem</span> 
     </div>
 }
 export default Lablec;
