3e4183aa32a8acb7989494e37c3fcc422df9a2f5
nemes <nemesmatyas@gmail.com>
2022-11-08 13:39:35 +0100

    Kezdőoldal és kártyák CSS fixálva

--- src/components/KezdooldalCard.js
+++ src/components/KezdooldalCard.js
@@ -1,7 +1,38 @@
+import { Link } from "react-router-dom";
+
 import classes from "./KezdooldalCard.module.css";
 
-const KezdooldalCard = props => {
+const KezdooldalCard = ({ linkToPage, cardDisplayText, imgRoute }) => {
+  // A dinamikus backgroundImage link miatt inline styles-t kell használni
+  const kezdooldalStyles = {
+    width: "350px",
+    height: "450px",
+    backgroundColor: "white",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundImage: `url(${imgRoute})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    borderRadius: "20px",
+    boxShadow:
+      "rgba(60, 66, 87, 0.12) 10px 7px 14px 0px, rgba(0, 0, 0, 0.12) 10px 3px 6px 0px",
+  };
 
-    return <div className={classes["kezdooldal-card"]}>{props.children}</div>
-}
+  return (
+    <div className={classes["kezdooldal-card-container"]}>
+      <div style={kezdooldalStyles}>
+        {console.log(`link to page: ${linkToPage}`)}
+        <div className={classes["kezdooldal-card-link-wrapper"]}>
+          <Link to={linkToPage} className={classes["kezdooldal-card-link"]}>
+            {cardDisplayText}
+            <span></span>
+            {/* A <span> kell ahhoz, hogy az egész kártya kattintható legyen */}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default KezdooldalCard;
